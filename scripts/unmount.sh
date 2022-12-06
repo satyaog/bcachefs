fabe4d00defc81dff5b9b6520d840c4ff90893da
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2022-12-06 15:15:49 -0800

    Add FORCE flag to unmount image

--- scripts/unmount.sh
+++ scripts/unmount.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -o errexit -o noclobber
 
-singularity exec instance://bcachefs fusermount3 -u /bch/mount/
+if [ -z "${FORCE}" ]
+then
+	FORCE=0
+fi
+
+singularity exec instance://bcachefs fusermount3 -u /bch/mount/ || [ ${FORCE} -eq 1 ]
 sleep 10
 singularity instance stop bcachefs
